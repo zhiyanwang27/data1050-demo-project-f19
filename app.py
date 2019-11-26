06bf9be4e2685b342e8a420d8a91a2828887a8b5
Bh Ma <blownhitherma@gmail.com>
2019-11-26 01:42:50 -0500

    [Feature] timeout cache in db layer

--- app.py
+++ app.py
@@ -6,12 +6,67 @@ import plotly.graph_objects as go
 
 from database import fetch_all_bpa_as_df
 
+# Definitions of constants. This projects uses extra CSS stylesheet at `./assets/style.css`
 COLORS = ['rgb(67,67,67)', 'rgb(115,115,115)', 'rgb(49,130,189)', 'rgb(189,189,189)']
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', '/assets/style.css']
+
+# Define the dash app first
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
+# Define component functions
+
+
+def page_header():
+    """
+    Returns the page header as a dash `html.Div`
+    """
+    return html.Div(id='header', children=[
+        html.Div([html.H3('Visualization with datashader and Plotly')],
+                 className="ten columns"),
+        html.A([html.Img(id='logo', src=app.get_asset_url('github.png'),
+                         style={'height': '35px', 'paddingTop': '7%'}),
+                html.Span('Blownhither', style={'fontSize': '2rem', 'height': '35px', 'bottom': 0,
+                                                'paddingLeft': '4px', 'color': '#a3a7b0',
+                                                'textDecoration': 'none'})],
+               className="two columns row",
+               href='https://github.com/blownhither/'),
+    ], className="row")
+
+
+def description():
+    """
+    Returns overall project description in markdown
+    """
+    return html.Div(children=[dcc.Markdown('''
+        # Energy Planner
+        As of today, 138 cities in the U.S. have formally announced 100% renewable energy goals or
+        targets, while others are actively considering similar goals. Despite ambition and progress,
+        conversion towards renewable energy remains challenging.
+
+        Wind and solar power are becoming more cost effective, but they will always be unreliable
+        and intermittent sources of energy. They follow weather patterns with potential for lots of
+        variability. Solar power starts to die away right at sunset, when one of the two daily peaks
+        arrives (see orange curve for load).
+
+        **Energy Planner is a "What-If" tool to assist making power conversion plans.**
+        It can be used to explore load satisfiability under different power contribution with 
+        near-real-time energy production & consumption data.
+
+        ### Data Source
+        Energy Planner utilizes near-real-time energy production & consumption data from [BPA 
+        Balancing Authority](https://www.bpa.gov/news/AboutUs/Pages/default.aspx).
+        The [data source](https://transmission.bpa.gov/business/operations/Wind/baltwg.aspx) 
+        **updates every 5 minutes**. 
+        ''', className='eleven columns', style={'paddingLeft': '5%'})], className="row")
+
+
 def static_stacked_trend_graph(stack=False):
+    """
+    Returns scatter line plot of all power sources and power load.
+    If `stack` is `True`, the 4 power sources are stacked together to show the overall power
+    production.
+    """
     df = fetch_all_bpa_as_df()
     if df is None:
         return go.Figure()
@@ -27,7 +82,6 @@ def static_stacked_trend_graph(stack=False):
     title = 'Energy Production & Consumption under BPA Balancing Authority'
     if stack:
         title += ' [Stacked]'
-
     fig.update_layout(template='plotly_dark',
                       title=title,
                       plot_bgcolor='#23272c',
@@ -37,47 +91,10 @@ def static_stacked_trend_graph(stack=False):
     return fig
 
 
-def page_header():
-    return html.Div(id='header', children=[
-        html.Div([html.H3('Visualization with datashader and Plotly')],
-                 className="ten columns"),
-        html.A([html.Img(id='logo', src=app.get_asset_url('github.png'),
-                         style={'height': '35px', 'paddingTop': '7%'}),
-                html.Span('Blownhither', style={'fontSize': '2rem', 'height': '35px', 'bottom': 0,
-                                                'paddingLeft': '4px', 'color': '#a3a7b0',
-                                                'textDecoration': 'none'})],
-               className="two columns row",
-               href='https://github.com/blownhither/'),
-    ], className="row")
-
-
-def description():
-    return html.Div(children=[
-        dcc.Markdown('''
-        # Energy Planner
-        As of today, 138 cities in the U.S. have formally announced 100% renewable energy goals or
-        targets, while others are actively considering similar goals. Despite ambition and progress,
-        conversion towards renewable energy remains challenging.
-        
-        Wind and solar power are becoming more cost effective, but they will always be unreliable
-        and intermittent sources of energy. They follow weather patterns with potential for lots of
-        variability. Solar power starts to die away right at sunset, when one of the two daily peaks
-        arrives (see orange curve for load).
-        
-        **Energy Planner is a "What-If" tool to assist making power conversion plans.**
-        It can be used to explore load satisfiability under different power contribution with 
-        near-real-time energy production & consumption data.
-        
-        ### Data Source
-        Energy Planner utilizes near-real-time energy production & consumption data from [BPA 
-        Balancing Authority](https://www.bpa.gov/news/AboutUs/Pages/default.aspx).
-        The [data source](https://transmission.bpa.gov/business/operations/Wind/baltwg.aspx) 
-        **updates every 5 minutes**. 
-        ''', className='eleven columns', style={'paddingLeft': '5%'})
-    ], className="row")
-
-
 def what_if_description():
+    """
+    Returns description of "What-If" - the interactive component
+    """
     return html.Div(children=[
         dcc.Markdown('''
         # " What If "
@@ -85,12 +102,18 @@ def what_if_description():
         Could our city survive an outage of hydro power and use up-scaled wind power as an
         alternative? Find below **what would happen with 2.5x wind power and no hydro power at 
         all**.   
-        Feel free to try out more combinations with the sliders.  
+        Feel free to try out more combinations with the sliders. For the clarity of demo code,
+        only two sliders are included here. A fully-functioning What-If tool should support
+        playing with other interesting aspects of the problem (e.g. instability of load).
         ''', className='eleven columns', style={'paddingLeft': '5%'})
     ], className="row")
 
 
 def what_if_tool():
+    """
+    Returns the What-If tool as a dash `html.Div`. The view is a 8:3 division between
+    demand-supply plot and rescale sliders.
+    """
     return html.Div(children=[
         html.Div(children=[dcc.Graph(id='what-if-figure')], className='nine columns'),
 
@@ -112,16 +135,19 @@ def what_if_tool():
     ], className='row eleven columns', style={'paddingLeft': '5%'})
 
 
-def app_summary():
+def architecture_summary():
+    """
+    Returns the text and image of architecture summary of the project.
+    """
     return html.Div(children=[
         dcc.Markdown('''
-        # Project Architecture
-        This project uses MongoDB as the database. All data acquired are stored in raw form to the
-        database (with de-duplication). An abstract layer is built in `database.py` so all queries
-        can be done via function call. For a more complicated app, the layer will also be
-        responsible for schema consistency. An `plot.ly` & `dash` app is serving this web page
-        through. Actions on responsive components on the page is redirected to `app.py` which will
-        then update certain components on the page.  
+            # Project Architecture
+            This project uses MongoDB as the database. All data acquired are stored in raw form to the
+            database (with de-duplication). An abstract layer is built in `database.py` so all queries
+            can be done via function call. For a more complicated app, the layer will also be
+            responsible for schema consistency. An `plot.ly` & `dash` app is serving this web page
+            through. Actions on responsive components on the page is redirected to `app.py` which will
+            then update certain components on the page.  
         ''', className='row eleven columns', style={'paddingLeft': '5%'}),
 
         html.Div(children=[
@@ -135,6 +161,9 @@ def app_summary():
     ], className='row')
 
 
+# Sequentially add page components to the app's layout
+
+
 app.layout = html.Div([
     page_header(),
     html.Hr(),
@@ -143,14 +172,17 @@ app.layout = html.Div([
     dcc.Graph(id='stacked-trend-graph', figure=static_stacked_trend_graph(stack=True)),
     what_if_description(),
     what_if_tool(),
-    app_summary(),
+    architecture_summary(),
 ], className='row', id='content')
 
+
+# Defines the dependencies of interactive components
 
 @app.callback(
     dash.dependencies.Output('wind-scale-text', 'children'),
     [dash.dependencies.Input('wind-scale-slider', 'value')])
 def update_wind_sacle_text(value):
+    """Changes the display text of the wind slider"""
     return f"Wind Power Scale {value:.2f}x"
 
 
@@ -158,6 +190,7 @@ def update_wind_sacle_text(value):
     dash.dependencies.Output('hydro-scale-text', 'children'),
     [dash.dependencies.Input('hydro-scale-slider', 'value')])
 def update_hydro_sacle_text(value):
+    """Changes the display text of the hydro slider"""
     return f"Hydro Power Scale {value:.2f}x"
 
 
@@ -169,15 +202,9 @@ _what_if_data_cache = None
     [dash.dependencies.Input('wind-scale-slider', 'value'),
      dash.dependencies.Input('hydro-scale-slider', 'value')])
 def what_if_handler(wind, hydro):
-    global _what_if_data_cache
-    if _what_if_data_cache is None:
-        _what_if_data_cache = fetch_all_bpa_as_df()
-        if _what_if_data_cache is None:
-            return go.Figure()
-
-    df = _what_if_data_cache.copy()
+    """Changes the display graph of supply-demand"""
+    df = fetch_all_bpa_as_df(allow_cached=True)
     x = df['Datetime']
-
     supply = df['Wind'] * wind + df['Hydro'] * hydro + df['Fossil/Biomass'] + df['Nuclear']
     load = df['Load']
 
@@ -186,7 +213,6 @@ def what_if_handler(wind, hydro):
                   fill='tozeroy'))
     fig.add_trace(go.Scatter(x=x, y=load, mode='none', name='demand', line={'width': 2, 'color': 'orange'},
                   fill='tonexty'))
-
     fig.update_layout(template='plotly_dark', title='Supply/Demand after Power Scaling',
                       plot_bgcolor='#23272c', paper_bgcolor='#23272c', yaxis_title='MW',
                       xaxis_title='Date/Time')
--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 import logging
 import pymongo
 import pandas as pds
+import expiringdict
 
 import utils
 
 client = pymongo.MongoClient()
 logger = logging.Logger(__name__)
 utils.setup_logger(logger, 'db.log')
+RESULT_CACHE_EXPIRATION = 10             # seconds
 
 
 def upsert_bpa(df):
@@ -33,13 +35,32 @@ def fetch_all_bpa():
     return list(collection.find())
 
 
-def fetch_all_bpa_as_df():
-    data = fetch_all_bpa()
-    if len(data) == 0:
-        return None
-    df = pds.DataFrame.from_records(data)
-    df.drop('_id', axis=1, inplace=True)
-    return df
+_fetch_all_bpa_as_df_cache = expiringdict.ExpiringDict(max_len=1,
+                                                       max_age_seconds=RESULT_CACHE_EXPIRATION)
+
+
+def fetch_all_bpa_as_df(allow_cached=False):
+    """Converts list of dicts returned by `fetch_all_bpa` to DataFrame with ID removed
+    Actual job is done in `_worker`. When `allow_cached`, attempt to retrieve timed cached from
+    `_fetch_all_bpa_as_df_cache`; ignore cache and call `_work` if cache expires or `allow_cached`
+    is False.
+    """
+    def _work():
+        data = fetch_all_bpa()
+        if len(data) == 0:
+            return None
+        df = pds.DataFrame.from_records(data)
+        df.drop('_id', axis=1, inplace=True)
+        return df
+
+    if allow_cached:
+        try:
+            return _fetch_all_bpa_as_df_cache['cache']
+        except KeyError:
+            pass
+    ret = _work()
+    _fetch_all_bpa_as_df_cache['cache'] = ret
+    return ret
 
 
 if __name__ == '__main__':
